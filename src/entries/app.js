3afcb7462902fa791fbd5f86a196597fa311ee60
Juan Pablo Calao Perez <jcalaop@gmail.com>
2019-02-20 18:45:21 -0500

    Clase Route
    Reto: Hacer el componente de Contacto, y de Perfil

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { hydrate } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import Home from '../pages/containers/home';
+import Videos from '../pages/containers/videos';
+import Home from '../pages/components/home';
 import reducer from '../reducer';
 import { Map as map } from 'immutable';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from '../pages/components/header';
 
 const store = createStore(
   reducer,
@@ -23,7 +25,11 @@ const homeContainer = document.getElementById('home-container')
 hydrate(
   <BrowserRouter>
     <Provider store={store}>
-      <Home />
+      <Fragment>
+        <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/videos" exact component={Videos} />
+      </Fragment>
     </Provider>
   </BrowserRouter>
   , homeContainer)
--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -1,11 +1,9 @@
 import React from 'react';
-// import logo from '../../../images/logo.png';
 import './related.css';
 
 function Related(props) {
   return (
     <div className="Related">
-      <img src="images/logo.png" width={250}/>
     </div>
   )
 }
