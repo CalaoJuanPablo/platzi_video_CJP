9ae8170010172837443bb30295c08852c64d6dc1
Juan Pablo Calao Perez <jcalaop@gmail.com>
2019-02-26 18:12:16 -0500

    Not Found and Redirect

---
+++ src/NotFound/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = props =>
+  <div className="NotFound">
+    <h1>Contenido no encontrado</h1>
+  </div>
+
+export default NotFound;
--- src/entries/app.js
+++ src/entries/app.js
@@ -9,10 +9,11 @@ import { Map as map } from 'immutable';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Header from '../pages/components/header';
 import Contacto from '../contacto/components/contacto';
 import Perfil from '../perfil/components/Perfil';
+import NotFound from '../NotFound/components/NotFound';
 
 const store = createStore(
   reducer,
@@ -29,10 +30,14 @@ hydrate(
     <Provider store={store}>
       <Fragment>
         <Header />
-        <Route path="/" exact component={Home} />
-        <Route path="/videos" exact component={Videos} />
-        <Route path="/contacto" exact component={Contacto} />
-        <Route path="/perfil" exact component={Perfil} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/videos" exact component={Videos} />
+          <Redirect from="/v" to="/videos" />
+          <Route path="/contacto" exact component={Contacto} />
+          <Route path="/perfil" exact component={Perfil} />
+          <Route component={NotFound} />
+        </Switch>
       </Fragment>
     </Provider>
   </BrowserRouter>
